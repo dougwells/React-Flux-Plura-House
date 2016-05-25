dcf5714b064b451696d723c80c586088071dcc38
dougwells <dougwells@gmail.com>
2016-05-25 08:18:35 -0600

    React-Router: Locations (traditional URL structure instead of hash)

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -48464,7 +48464,24 @@ var React = require('react');
 
 
 var About = React.createClass({displayName: "About",
-	render: function(){
+	statics: {
+        willTransitionTo: function(transition, params, query, callback) {
+            if (!confirm('Are you sure you want to read a page that\'s this boring?')) {
+                    transition.about(); 
+            } else {
+                callback();
+            }       
+        },
+
+        willTransitionFrom: function(transition, component) {
+            if (!confirm('Are you sure you want to leave a page that\'s this exciting?')) {
+                    transition.about();        
+            }
+        }
+
+    },
+
+    render: function(){
 		return (
 
             React.createElement("div", null, 
@@ -48661,7 +48678,7 @@ var Router = require('react-router');
 var routes = require('./routes');
 
 
-Router.run(routes, function(Handler){
+Router.run(routes, Router.HistoryLocation, function(Handler){
 	React.render(React.createElement(Handler, null), document.getElementById('app'));
 });
 
