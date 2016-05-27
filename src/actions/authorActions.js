f2f0a4d6278ef64a965d76bde6627ba0bd946d59
dougwells <dougwells@gmail.com>
2016-05-27 11:43:23 -0600

    React Flux - Actions and Dispatcher

---
+++ src/actions/authorActions.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var Dispatcher = require('../dispatcher/appDispatcher');
+var AuthorApi = require('../api/authorapi');
+var ActionTypes = require('../constants/actionTypes');
+
+var AuthorActions = {
+  createAuthor = function(author){
+    var newAuthor = AuthorApi.saveAuthor(author);
+
+    //hey dispatcher, go tell all the stores that an author was created ...
+    Dispatcher.dispatch({
+      actionType: ActionTpes.CREATE_AUTHOR,
+      author: newAuthor
+    });
+  }
+};
+
+module.exports = AuthorActions;
